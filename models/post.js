ce6c5fb5df0f82d3ac436ae50d62e73126cad789
liujjxi <liujjxi@qq.com>
2015-10-23 14:57:51 +0800

    编辑与删除

--- models/post.js
+++ models/post.js
@@ -117,3 +117,88 @@ Post.getOne=function(name,day,title,callback){
     });
   });
 };
+Post.edit=function(name,day,title,callback){
+  //打开数据库
+  mongodb.open(function(err,db){
+    if(err){
+      return callback(err);
+    }
+    //读取posts集合
+    db.collection('posts',function(err,collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名，日期及文章名进行查询
+      collection.findOne({
+        "name":name,
+        "time.day":day,
+        "title":title
+      },function(err,doc){
+        mongodb.close();
+        if(err){
+          return callback(err);
+        }
+        callback(null,doc);
+      })
+    })
+  })
+}
+Post.update=function(name,day,title,post,callback){
+  //打开数据库
+  mongodb.open(function(err,db){
+    if(err){
+      return callback(err);
+    }
+    //读取posts集合
+    db.collection('posts',function(err,collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名，日期及文章名进行查询
+      collection.update({
+        "name":name,
+        "time.day":day,
+        "title":title
+      },{
+        $set:{post:post}
+      },function(err,doc){
+        mongodb.close();
+        if(err){
+          return callback(err);
+        }
+        callback(null);
+      })
+    })
+  })
+}
+Post.remove=function(name,day,title,callback){
+  //打开数据库
+  mongodb.open(function(err,db){
+    if(err){
+      return callback(err);
+    }
+    //读取posts集合
+    db.collection('posts',function(err,collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名，日期及文章名进行查询
+      collection.remove({
+        "name":name,
+        "time.day":day,
+        "title":title
+      },{
+        w:1
+      },function(err,doc){
+        mongodb.close();
+        if(err){
+          return callback(err);
+        }
+        callback(null);
+      })
+    })
+  })
+}
